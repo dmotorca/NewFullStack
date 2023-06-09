6553d02a538785b32182e558d26f80f1e473ffc3
David Motorca <dmotorca@pdx.edu>
2023-06-09 14:58:08 -0400

    Added IO and socket code to handle clients joining and messaging. I used the message route that we already got working for doggr

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,16 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors';
 import multipart from '@fastify/multipart';
+import {Message} from "./db/entities/Message.js";
 import { AuthPlugin } from "./plugins/auth.js";
 import { FastifyBadWordsPlugin } from "./plugins/badwords.js";
 import { FastifySearchHttpMethodPlugin } from "./plugins/http_search.js";
 import { FastifyMikroOrmPlugin } from "./plugins/mikro.js";
 import DoggrRoutes from "./routes/routes.js";
+// ***** My Code *****
+import { Server as SocketIOServer} from 'socket.io';
+import { createServer } from 'http';
+// ***** My Code *****
 import config from "./db/mikro-orm.config.js";
 
 const envToLogger = {
@@ -61,5 +66,34 @@ await app.register(FastifySearchHttpMethodPlugin, {});
 await app.register(FastifyBadWordsPlugin);
 await app.register(AuthPlugin);
 await app.register(DoggrRoutes, {});
+
+
+// ***** My Code *****
+const io = new SocketIOServer(app.server, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+		allowedHeaders: "*"
+	}
+});
+
+// ***** My Code IO STUFF *****
+io.on("connection", (socket) => {
+	console.log("New client connected");
+	
+	// Handle socket events
+	socket.on("chatMessage", (message) => {
+		console.log("Received chat message:", message);
+		
+		// Broadcast the message to all connected clients
+		io.emit("chatMessage", message);
+	});
+	
+	socket.on("disconnect", () => {
+		console.log("Client disconnected");
+	});
+});
+
+// ***** My Code IO STUFF *****
 
 export default app;
